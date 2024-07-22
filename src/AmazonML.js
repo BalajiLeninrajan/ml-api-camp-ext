3dbfee54cb833442945c668188d3c8ff82a9eabe
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-07-22 22:00:57 +0700

    Minor bug fix

--- src/AmazonML.js
+++ src/AmazonML.js
@@ -13,7 +13,6 @@ const creds = {
     credentials: {
         accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
         secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-        sessionToken: import.meta.env.VITE_AWS_SESSION_TOKEN, // leave out if not in hosted workshop
     },
 };
 
